412ba9d6f7421f933bd10d5592873297825553e2
Sérgio Gonçalves <sergiojoao3240@gmail.com>
2023-05-31 10:58:03 +0100

    Last changes

--- Project/Visual/SearchPage/script.js
+++ Project/Visual/SearchPage/script.js
@@ -1,7 +1,5 @@
-// Get the button:
 let mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -12,8 +10,42 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-} 
+  document.documentElement.scrollTop = 0;
+}
+
+
+
+
+ // Função para exibir o overlay de pesquisa
+ function showOverlay() {
+  document.getElementById("overlay").style.display = "block";
+}
+
+// Função para ocultar o overlay de pesquisa
+function hideOverlay() {
+  document.getElementById("overlay").style.display = "none";
+}
+
+// Função para realizar a pesquisa
+function searchElements() {
+  var searchValue = document.getElementById("searchInput").value.toLowerCase();
+  var gameElements = document.getElementsByClassName("game");
+
+  for (var i = 0; i < gameElements.length; i++) {
+    var titleElement = gameElements[i].getElementsByClassName("title")[0];
+    var title = titleElement.innerText.toLowerCase();
+
+    if (title.includes(searchValue)) {
+      gameElements[i].style.display = "block";
+    } else {
+      gameElements[i].style.display = "none";
+    }
+  }
+  hideOverlay();
+}
+
+
+// Adicionar manipuladores de eventos aos botões
+document.getElementById("searchBtn").addEventListener("click", showOverlay);
+document.getElementById("searchSubmit").addEventListener("click", searchElements);
